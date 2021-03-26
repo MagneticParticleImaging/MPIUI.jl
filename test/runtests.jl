e126132b9b7d1f102f6b6bf50295702d2894ec7a
mboberg <m.boberg@uke.de>
2021-03-26 14:23:21 +0000

    add tests for SFViewer

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,31 @@
 using MPIUI
 using Test
+using HTTP, ImageMagick
+using Gtk, Cairo
 
-# write your own tests here
-@test 1 == 1
+# download test data
+mkpath("./data/")
+function download_(filenameServer, filenameLocal)
+    if !isfile(filenameLocal)
+      @info "download $(filenameLocal)..."
+      HTTP.open("GET", "http://media.tuhh.de/ibi/openMPIData/data/"*filenameServer) do http
+        open(filenameLocal, "w") do file
+          write(file, http)
+        end
+      end
+    end
+end
+
+# compare two images
+mkpath("img/")
+mkpath("correct/")
+macro testImg(filename)
+    return :(
+      im1 = ImageMagick.load(joinpath("img", $filename));
+      im2 = ImageMagick.load(joinpath("correct", $filename));
+      @test im1 == im2
+      )
+end
+
+# tests
+include("SFViewer.jl")
