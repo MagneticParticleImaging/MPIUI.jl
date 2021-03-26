781a247fe45fbb6f06c014deadcbc67508b62918
Tobias Knopp <tobias@knoppweb.de>
2021-03-27 00:07:56 +0100

    enable test with better approximation

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,11 +19,17 @@ end
 # compare two images
 mkpath("img/")
 mkpath("correct/")
+
+# The image comparison takes into account that the test can run on different
+# computers with a differen resolution. For this reason the test images are
+# resized to a common size. Since this involves interpolation we need to take
+# interpolation errors into account and therefore allow an error of 20%
 macro testImg(filename)
     return :(
       im1 = ImageMagick.load(joinpath("img", $filename));
       im2 = imresize( ImageMagick.load(joinpath("correct", $filename)), size(im1));
-      #@test isapprox(im1,im2,atol=0.001)
+      d = im1-im2;
+      @test (norm(red.(d),1) + norm(green.(d),1) + norm(blue.(d),1)) / (3*length(im1)) < 0.2
       )
 end
 
