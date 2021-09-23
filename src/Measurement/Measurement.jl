57fdcc29defe024757473d977788dc7a45078d10
nHackel <nHackel@users.noreply.github.com>
2021-09-23 11:07:29 +0200

    Moved the if-EventType construction into a dispatch based approach for async measurement

--- src/Measurement/Measurement.jl
+++ src/Measurement/Measurement.jl
@@ -1,5 +1,9 @@
-
-
+abstract type EventType end
+struct UnwantedEvent <: EventType end
+struct WantedEvent <: EventType end
+EventType(m::MeasurementWidget, event::ProtocolEvent) = UnwantedEvent()
+EventType(m::MeasurementWidget, event::ProgressEvent) = typeof(m.protocolStatus.waitingOnReply) == ProgressQueryEvent ? WantedEvent() : UnwantedEvent()
+EventType(m::MeasurementWidget, event::DataAnswerEvent) = event.query == m.protocolStatus.waitingOnReply ? WantedEvent() : UnwantedEvent()
 
 function measurement(widgetptr::Ptr, m::MeasurementWidget)
     try
@@ -30,61 +34,11 @@ function displayMeasurement(m::MeasurementWidget, timerMeas::Timer)
     seq = m.scanner.currentSequence
     channel = m.biChannel
     deltaT = ustrip(u"s", dfCycle(seq) / rxNumSamplesPerPeriod(seq))
-    fr = 0
+    finished = false
 
     if isready(channel)
       event = take!(channel)
-      if event isa FinishedNotificationEvent
-        # We noticed end and overwrite the query we are waiting for
-        @info "Received finish notification"
-        bufferRequest = DataQueryEvent("BUFFER")
-        put!(channel, bufferRequest)
-        m.protocolStatus.waitingOnReply = bufferRequest  
-      elseif event isa DataAnswerEvent && event.query == m.protocolStatus.waitingOnReply
-        # We were waiting on the last buffer request
-        if event.query.message == "BUFFER"
-          @info "Finishing measurement"
-          close(timerMeas)
-          infoMessage(m, "", "green")    
-          params = getParams(m)
-          bgdata = length(m.dataBGStore) == 0 ? nothing : m.dataBGStore 
-          buffer = event.data
-          m.filenameExperiment = MPIFiles.saveasMDF(m.mdfstore, m.scanner, buffer, params; bgdata=bgdata)
-          updateData(m.rawDataWidget, m.filenameExperiment)
-          updateExperimentStore(mpilab[], mpilab[].currentStudy)
-          put!(channel, FinishedAckEvent())
-        # We were waiting on a new frame
-        elseif startswith(event.query.message, "FRAME")
-          @info "Received frame"
-          frame = event.data
-          if !isnothing(frame)
-            infoMessage(m, "$(m.progress.unit) $(m.progress.done) / $(m.progress.total)", "green")
-            if get_gtk_property(m["cbOnlinePlotting",CheckButtonLeaf], :active, Bool)
-              updateData(m.rawDataWidget, frame, deltaT)
-            end
-          end
-          # Ask for next progress
-          progressQuery = ProgressQueryEvent()
-          put!(channel, progressQuery)
-          m.protocolStatus.waitingOnReply = progressQuery
-        end
-      elseif event isa ProgressEvent && typeof(m.protocolStatus.waitingOnReply) == ProgressQueryEvent
-        # New Progress noticed
-        if isnothing(m.progress) || m.progress != event
-          @info "New progress, asking for frame $(event.done)"
-          m.progress = event
-          dataQuery = DataQueryEvent("FRAME:$(event.done)")
-          put!(channel, dataQuery)
-          m.protocolStatus.waitingOnReply = dataQuery
-        else
-          # Ask for next progress
-          progressQuery = ProgressQueryEvent()
-          put!(channel, progressQuery)
-          m.protocolStatus.waitingOnReply = progressQuery
-        end
-      else
-        @info "Discard event $(typeof(event))"
-      end
+      finished = handleAsyncEvent(m, event, EventType(m, event))
     end
 
     if isnothing(m.protocolStatus.waitingOnReply)
@@ -94,11 +48,81 @@ function displayMeasurement(m::MeasurementWidget, timerMeas::Timer)
       m.protocolStatus.waitingOnReply = progressQuery
     end
 
+    if finished 
+      close(timerMeas)
+    end
+
   catch ex
     close(timerMeas)
     showError(ex)
   end
 end
+
+function handleAsyncEvent(m::MeasurementWidget, event::FinishedNotificationEvent, ::UnwantedEvent)
+  channel = m.biChannel
+  # We noticed end and overwrite the query we are waiting for
+  @info "Received finish notification"
+  bufferRequest = DataQueryEvent("BUFFER")
+  put!(channel, bufferRequest)
+  m.protocolStatus.waitingOnReply = bufferRequest
+  return false
+end
+
+function handleAsyncEvent(m::MeasurementWidget, event::DataAnswerEvent, ::WantedEvent)
+  channel = m.biChannel
+  # We were waiting on the last buffer request
+  if event.query.message == "BUFFER"
+    @info "Finishing measurement"
+    infoMessage(m, "", "green")    
+    params = getParams(m)
+    bgdata = length(m.dataBGStore) == 0 ? nothing : m.dataBGStore 
+    buffer = event.data
+    m.filenameExperiment = MPIFiles.saveasMDF(m.mdfstore, m.scanner, buffer, params; bgdata=bgdata)
+    updateData(m.rawDataWidget, m.filenameExperiment)
+    updateExperimentStore(mpilab[], mpilab[].currentStudy)
+    put!(channel, FinishedAckEvent())
+    return true
+  # We were waiting on a new frame
+  elseif startswith(event.query.message, "FRAME")
+    @info "Received frame"
+    frame = event.data
+    if !isnothing(frame)
+      infoMessage(m, "$(m.progress.unit) $(m.progress.done) / $(m.progress.total)", "green")
+      if get_gtk_property(m["cbOnlinePlotting",CheckButtonLeaf], :active, Bool)
+        updateData(m.rawDataWidget, frame, deltaT)
+      end
+    end
+    # Ask for next progress
+    progressQuery = ProgressQueryEvent()
+    put!(channel, progressQuery)
+    m.protocolStatus.waitingOnReply = progressQuery
+  end
+  return false
+end
+
+function handleAsyncEvent(m::MeasurementWidget, event::ProgressEvent, ::WantedEvent)
+  channel = m.biChannel
+  # New Progress noticed
+  if isnothing(m.progress) || m.progress != event
+    @info "New progress, asking for frame $(event.done)"
+    m.progress = event
+    dataQuery = DataQueryEvent("FRAME:$(event.done)")
+    put!(channel, dataQuery)
+    m.protocolStatus.waitingOnReply = dataQuery
+  else
+    # Ask for next progress
+    progressQuery = ProgressQueryEvent()
+    put!(channel, progressQuery)
+    m.protocolStatus.waitingOnReply = progressQuery
+  end
+  return false
+end
+
+function handleAsyncEvent(m::MeasurementWidget, event::ProtocolEvent, ::UnwantedEvent)
+  @info "Discard event $(typeof(event))"
+  return false
+end
+
 
 
 function measurementBG(widgetptr::Ptr, m::MeasurementWidget)
