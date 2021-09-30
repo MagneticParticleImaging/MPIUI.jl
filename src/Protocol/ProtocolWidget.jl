01220921f6b65945c7f68228095d96e372a99a21
nHackel <nHackel@users.noreply.github.com>
2021-09-30 17:00:13 +0200

    Some more work on ProtocolWidget, implemented Protocol changing and updating type and description but not yet parameters

--- src/Protocol/ProtocolWidget.jl
+++ src/Protocol/ProtocolWidget.jl
@@ -2,6 +2,7 @@ mutable struct ProtocolWidget{T} <: Gtk.GtkBox
     # Gtk 
     handle::Ptr{Gtk.GObject}
     builder::GtkBuilder
+    paramBuilder::Dict
     # Protocol Interaction
     scanner::T
     protocol::Union{Protocol, Nothing}
@@ -31,7 +32,9 @@ function ProtocolWidget(scanner=nothing)
     b = Builder(filename=uifile)
     mainBox = object_(b, "boxProtocol",BoxLeaf)
 
-    pw = ProtocolWidget(mainBox.handle, b, scanner, protocol, nothing, nothing, 
+    paramBuilder = Dict(:sequence => "expSequence", :positions => "expPositions")
+
+    pw = ProtocolWidget(mainBox.handle, b, paramBuilder, scanner, protocol, nothing, nothing, 
         mdfstore, zeros(Float32,0,0,0,0), "", now())
     Gtk.gobject_move_ref(pw, mainBox)
 
@@ -39,11 +42,13 @@ function ProtocolWidget(scanner=nothing)
         @idle_add begin
             set_gtk_property!(pw["tbStart",ToolButtonLeaf],:sensitive,false)
             set_gtk_property!(pw["tbPause",ToolButtonLeaf],:sensitive,false)
-            set_gtk_property!(pw["tbCancel",ToggleToolButtonLeaf],:sensitive,false)      
+            set_gtk_property!(pw["tbCancel",ToolButtonLeaf],:sensitive,false)      
+            set_gtk_property!(pw["tbRestart",ToolButtonLeaf],:sensitive,false)      
         end
     else
         # Load default protocol and set params
         # + dummy plotting?
+        initProtocolChoices(pw)
         initCallbacks(pw)
     end
     @info "Finished starting ProtocolWidget"
@@ -51,7 +56,80 @@ function ProtocolWidget(scanner=nothing)
 end
 
 function initCallbacks(pw::ProtocolWidget)
+    signal_connect(pw["tbRun", ToolButtonLeaf], :clicked) do w
+        #startProtocol(pw)
+    end
 
+    signal_connect(pw["tbPause", ToolButtonLeaf], :clicked) do w
+        #tryPauseProtocol(pw)
+    end
+
+    signal_connect(pw["tbCancel", ToolButtonLeaf], :clicked) do w
+        #tryCancelProtocol(pw)
+    end
+
+    signal_connect(pw["tbRestart", ToolButtonLeaf], :clicked) do w
+        #tryRestartProtocol(pw)
+    end
+
+    signal_connect(pw["cmbProtocolSelection", GtkComboBoxText], :changed) do w
+        protocolName = Gtk.bytestring( GAccessor.active_text(pw["cmbProtocolSelection", GtkComboBoxText])) 
+        #get_gtk_property(pw["cmbProtocolSelection", GtkComboBoxText], :active, AbstractString)
+        protocol = Protocol(protocolName, pw.scanner)
+        updateProtocol(pw, protocol)
+    end
+end
+
+function initProtocolChoices(pw::ProtocolWidget)
+    scanner = pw.scanner
+    choices = getProtocolList(scanner)
+    cb = pw["cmbProtocolSelection", GtkComboBoxText]
+    for choice in choices
+        push!(cb, choice)
+    end
+    defaultIndex = findfirst(x -> x == scanner.generalParams.defaultProtocol, choices)
+    if !isnothing(defaultIndex)
+        protocolName = choices[defaultIndex]
+        protocol = Protocol(protocolName, pw.scanner)
+        @idle_add begin
+            updateProtocol(pw, protocol)
+            set_gtk_property!(cb, :active, defaultIndex)
+        end
+    end
+end
+
+function updateProtocol(pw::ProtocolWidget, protocol::Protocol)
+    params = protocol.params
+    @info "Updating protocol"
+    @idle_add begin
+        set_gtk_property!(pw["lblProtocolType", GtkLabelLeaf], :label, string(typeof(protocol)))
+        set_gtk_property!(pw["txtBuffProtocolDescription", GtkTextBufferLeaf], :text, MPIMeasurements.description(protocol))
+    end
+    @idle_add begin 
+        for field in fieldnames(typeof(params))
+        @info "Try adding field $field"
+        try 
+            addProtocolParameter(pw, field, params)
+            @info "Added $field"
+        catch ex
+            @error ex
+        end
+        end
+    end
+end
+
+function addProtocolParameter(pw::ProtocolWidget, field, params)
+    if haskey(pw.paramBuilder, field)
+        @info "Do special things for parameter $field"
+    else 
+        gridGeneric = object_(pw.builder, "gridGeneric",GtkGrid)
+        labelLeaf = gridGeneric[1, 1]
+        valueEntry = gridGeneric[2, 1]
+        set_gtk_property!(labelLeaf, :label, string(field))
+        set_gtk_property!(valueEntry, :text, string(getfield(params, field)))
+        #TODO set entry name/id to fetch it later
+        push!(pw["boxProtocolParameter", BoxLeaf], gridGeneric)
+    end
 end
 
 function isMeasurementStore(m::ProtocolWidget, d::DatasetStore)
