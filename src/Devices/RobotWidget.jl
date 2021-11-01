88e66af5a5d99d4568f5a819bcfa5236d50354b2
nHackel <nHackel@users.noreply.github.com>
2021-11-01 14:44:26 +0100

    Implemented Coordinate Systems in RobotWidget

--- src/Devices/RobotWidget.jl
+++ src/Devices/RobotWidget.jl
@@ -5,20 +5,22 @@ mutable struct RobotWidget <: Gtk.GtkBox
   updating::Bool
   robot::Robot
   timer::Union{Timer, Nothing}
+  coordTransferfunction::Function
+  coordType::Union{Type{ScannerCoords}, Type{RobotCoords}}
   namedPos::Union{String, Nothing}
 end
 
 getindex(m::RobotWidget, w::AbstractString) = G_.object(m.builder, w)
 
 
-
+#TODO make usable for different dof
 function RobotWidget(robot::Robot)
   uifile = joinpath(@__DIR__,"..","builder","robotWidget.ui")
 
   b = Builder(filename=uifile)
   mainBox = G_.object(b, "mainBox")
 
-  m = RobotWidget(mainBox.handle, b, false, robot, nothing, nothing)
+  m = RobotWidget(mainBox.handle, b, false, robot, nothing, toScannerCoords, ScannerCoords, nothing)
   Gtk.gobject_move_ref(m, mainBox)
 
   if :namedPositions in fieldnames(typeof(params(robot)))
@@ -29,16 +31,12 @@ function RobotWidget(robot::Robot)
       end
       set_gtk_property!(m["cmbNamedPos"], :sensitive, true)
       set_gtk_property!(m["btnMoveNamedPos"], :sensitive, true)
+      displayNamedPosition(m)
     end
   end
 
   @idle_add begin
-    ranges = axisRange(m.robot)
-    # adapt for other dof
-    set_gtk_property!(m["entRangeX"], :text, string(ustrip.(u"mm", ranges[1])))
-    set_gtk_property!(m["entRangeY"], :text, string(ustrip.(u"mm", ranges[2])))
-    set_gtk_property!(m["entRangeZ"], :text, string(ustrip.(u"mm", ranges[3])))
-    set_gtk_property!(m["entMoveOrder"], :text, MPIMeasurements.movementOrder(m.robot))
+    displayAxisRange(m)
   end
 
   initCallbacks(m)
@@ -72,17 +70,7 @@ function initCallbacks(m::RobotWidget)
   end
   
   signal_connect(m["cmbNamedPos"], :changed) do w
-    @idle_add begin
-      m.namedPos = Gtk.bytestring(GAccessor.active_text(m["cmbNamedPos"]))
-      pos = namedPosition(m.robot, m.namedPos)
-      if length(pos) == 3
-        @idle_add begin
-          set_gtk_property!(m["entNamedPosX"], :text, string(ustrip(u"mm", pos[1])))
-          set_gtk_property!(m["entNamedPosY"], :text, string(ustrip(u"mm", pos[2])))
-          set_gtk_property!(m["entNamedPosZ"], :text, string(ustrip(u"mm", pos[3])))
-        end
-      end
-    end
+    displayNamedPosition(m)
   end
 
   signal_connect(m["btnMoveNamedPos"], :clicked) do w
@@ -92,6 +80,23 @@ function initCallbacks(m::RobotWidget)
       showError(ex)
     end
   end
+
+  signal_connect(m["radScannerCoord"], :toggled) do w
+    if get_gtk_property(w, :active, Bool)
+      m.coordTransferfunction = toScannerCoords
+      updatePositionCoords(m, toScannerCoords, m.coordType)
+      m.coordType = ScannerCoords
+    end
+  end
+
+  signal_connect(m["radRobotCoord"], :toggled) do w
+    if get_gtk_property(w, :active, Bool)
+      m.coordTransferfunction = toRobotCoords
+      updatePositionCoords(m, toRobotCoords, m.coordType)
+      m.coordType = RobotCoords
+    end
+  end
+
 end
 
 function displayRobotState(m::RobotWidget)
@@ -100,12 +105,102 @@ function displayRobotState(m::RobotWidget)
   end
 end
 
-function displayPosition(m::RobotWidget)
+function floatToPos(m::RobotWidget, posFloat::Vector{Float64})
+  return floatToPos(m, posFloat, m.coordType)
+end
+function floatToPos(m::RobotWidget, posFloat::Vector{Float64}, type::Union{Type{ScannerCoords}, Type{RobotCoords}})
+  if length(posFloat) != 3
+      return
+  end
+  pos = posFloat.*1Unitful.mm
+  return type(pos)
+end
+
+function currentPosition(m::RobotWidget, type=m.coordType)
+  entryX = get_gtk_property(m["entCurrPosX"], :text, String)
+  entryY = get_gtk_property(m["entCurrPosY"], :text, String)
+  entryZ = get_gtk_property(m["entCurrPosZ"], :text, String)
+  posFloat = tryparse.(Float64, [entryX, entryY, entryZ])
+  return any(isnothing, posFloat) ? nothing : floatToPos(m, posFloat, type)
+end
+
+# Always used current robotwidget type
+function movePosition(m::RobotWidget)
+  entryX = get_gtk_property(m["entMovePosX"], :text, String)
+  entryY = get_gtk_property(m["entMovePosY"], :text, String)
+  entryZ = get_gtk_property(m["entMovePosZ"], :text, String)
+  posFloat = tryparse.(Float64, [entryX, entryY, entryZ])
+  return any(isnothing, posFloat) ? nothing : floatToPos(m, posFloat)
+end
+
+function namedPosition(m::RobotWidget, type=m.coordType)
+  entryX = get_gtk_property(m["entNamedPosX"], :text, String)
+  entryY = get_gtk_property(m["entNamedPosY"], :text, String)
+  entryZ = get_gtk_property(m["entNamedPosZ"], :text, String)
+  posFloat = tryparse.(Float64, [entryX, entryY, entryZ])
+  return any(isnothing, posFloat) ? nothing : floatToPos(m, posFloat, type)
+end
+
+function displayNamedPosition(m::RobotWidget, pos::Nothing)
+  # NOP
+end
+function displayNamedPosition(m::RobotWidget, pos)
   @idle_add begin
-    pos = getPosition(m.robot)
+    set_gtk_property!(m["entNamedPosX"], :text, string(ustrip(u"mm", pos[1])))
+    set_gtk_property!(m["entNamedPosY"], :text, string(ustrip(u"mm", pos[2])))
+    set_gtk_property!(m["entNamedPosZ"], :text, string(ustrip(u"mm", pos[3])))
+  end
+end
+function displayNamedPosition(m::RobotWidget)
+  m.namedPos = Gtk.bytestring(GAccessor.active_text(m["cmbNamedPos"]))
+  pos = m.coordTransferfunction(m.robot, MPIMeasurements.namedPosition(m.robot, m.namedPos))
+  @show pos
+  if length(pos) == 3
+    displayNamedPosition(m, pos)
+  end
+end
+
+function displayCurrentPosition(m::RobotWidget, pos::Nothing)
+  # NOP
+end
+function displayCurrentPosition(m::RobotWidget, pos)
+  @idle_add begin
     set_gtk_property!(m["entCurrPosX"], :text, string(ustrip(u"mm", pos[1])))
     set_gtk_property!(m["entCurrPosY"], :text, string(ustrip(u"mm", pos[2])))
     set_gtk_property!(m["entCurrPosZ"], :text, string(ustrip(u"mm", pos[3])))
+  end
+end
+function displayCurrentPosition(m::RobotWidget)
+  pos = m.coordTransferfunction(m.robot, getPosition(m.robot))
+  displayCurrentPosition(m, pos)
+end
+
+function displayAxisRange(m::RobotWidget)
+  tempRanges = axisRange(m.robot)
+  # adapt for other dof
+  minPos = m.coordTransferfunction(m.robot, RobotCoords([tempRanges[i][1] for i = 1:3]))
+  maxPos = m.coordTransferfunction(m.robot, RobotCoords([tempRanges[i][2] for i = 1:3]))
+  ranges = [[minPos[i], maxPos[i]] for i = 1:3]
+  set_gtk_property!(m["entRangeX"], :text, string(ustrip.(u"mm", ranges[1])))
+  set_gtk_property!(m["entRangeY"], :text, string(ustrip.(u"mm", ranges[2])))
+  set_gtk_property!(m["entRangeZ"], :text, string(ustrip.(u"mm", ranges[3])))
+  set_gtk_property!(m["entMoveOrder"], :text, MPIMeasurements.movementOrder(m.robot))
+end
+
+function updatePositionCoords(m::RobotWidget, transferNewType::Function, oldType::Union{Type{ScannerCoords}, Type{RobotCoords}})
+  @idle_add begin
+    currPosOld = currentPosition(m, oldType)
+    if !isnothing(currPosOld)
+      currPosNew = transferNewType(m.robot, currPosOld)
+      displayCurrentPosition(m, currPosNew)
+    end
+    namedPosOld = namedPosition(m, oldType)
+    @show namedPosOld
+    if !isnothing(namedPosOld)
+      namedPosNew = transferNewType(m.robot, namedPosOld)
+      displayNamedPosition(m, namedPosNew)
+    end
+    displayAxisRange(m)
   end
 end
 
@@ -123,22 +218,16 @@ function robotMove(m::RobotWidget)
   return
   end
 
-  entryX = get_gtk_property(m["entMovePosX"], :text, String)
-  entryY = get_gtk_property(m["entMovePosY"], :text, String)
-  entryZ = get_gtk_property(m["entMovePosZ"], :text, String)
-  posFloat = tryparse.(Float64, [entryX, entryY, entryZ])
-
-  if any(posFloat .== nothing) || length(posFloat) != 3
-      return
+  pos = movePosition(m)
+  if !isnothing(pos)
+    @info "enabeling robot"
+    enable(m.robot)
+    @info "move robot"
+    moveAbs(m.robot, pos)
+    @info "move robot done"
+    disable(m.robot)
+    displayCurrentPosition(m)
   end
-  pos = posFloat.*1Unitful.mm
-  @info "enabeling robot"
-  enable(m.robot)
-  @info "move robot"
-  moveAbs(m.robot, pos)
-  @info "move robot done"
-  disable(m.robot)
-  displayPosition(m)
 end
 
 function moveNamedPosition(m::RobotWidget, posName::String)
@@ -152,7 +241,7 @@ function moveNamedPosition(m::RobotWidget, posName::String)
   MPIMeasurements.gotoPos(m.robot, posName)
   @info "move robot done"
   disable(m.robot)
-  displayPosition(m)
+  displayCurrentPosition(m)
 end
   
 
@@ -169,7 +258,7 @@ function referenceDrive(m::RobotWidget)
         enable(robot)
         doReferenceDrive(robot)
         disable(robot)
-        displayPosition(m)
+        displayCurrentPosition(m)
         message = """The robot is now referenced.
             You can mount your sample. Press \"Close\" to proceed. """
         info_dialog(message, mpilab[]["mainWindow"])
