14c088a51fcb0b7ba549d877e505aa826f1a2bbb
Tobias Knopp <tobias@knoppweb.de>
2021-11-18 14:23:06 +0100

    fix redraw problem with canvas

--- src/Devices/SurveillanceWidget.jl
+++ src/Devices/SurveillanceWidget.jl
@@ -131,6 +131,7 @@ function startSurveillanceUnit(m::SurveillanceWidget)
             end
             #Winston.xlabel("Time")
             display(m.canvas ,p)
+            m.canvas.is_sized = true
           end
         end
       end
