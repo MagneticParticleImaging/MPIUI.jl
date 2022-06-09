26768dfb711e398cad67caacd5a3b29f45856f3c
nHackel <nHackel@users.noreply.github.com>
2022-06-09 16:42:05 +0200

    Add 'hacky' fgPos paramater

---
+++ src/Protocol/CoordinateParameter.jl
@@ -0,0 +1,46 @@
+export CoordinateParameter
+
+mutable struct CoordinateParameter <: Gtk.GtkGrid
+  handle::Ptr{Gtk.GObject}
+  builder::GtkBuilder
+  field::Symbol
+
+  function CoordinateParameter(field::Symbol, coord::Union{ScannerCoords, Nothing}, tooltip::Union{Nothing, AbstractString} = nothing)
+    grid = GtkGrid()
+    uifile = joinpath(@__DIR__, "..", "builder", "positionsWidget.ui")
+    b = Builder(filename=uifile)
+    coordEntry = G_.object(b, "gridCoord")
+    label = GtkLabel(string(field))
+    set_gtk_property!(label, :xalign, 0.0)
+    addTooltip(label, tooltip)
+    grid[1, 1] = label
+    grid[2, 1] = coordEntry
+    coordParam = new(grid.handle, b, field)
+    updateCoordinate(coordParam, coord)
+    return Gtk.gobject_move_ref(coordParam, grid)
+  end
+end
+
+getindex(m::CoordinateParameter, w::AbstractString) = G_.object(m.builder, w)
+
+function updateCoordinate(param::CoordinateParameter, coord::ScannerCoords)
+  @idle_add begin
+    set_gtk_property!(param["entX"], :text, string(ustrip(u"mm", coord.data[1])))
+    set_gtk_property!(param["entY"], :text, string(ustrip(u"mm", coord.data[2])))
+    set_gtk_property!(param["entZ"], :text, string(ustrip(u"mm", coord.data[3])))
+  end
+end
+
+function updateCoordinate(param::CoordinateParameter, ::Nothing)
+  coord = ScannerCoords([0.0, 0.0, 0.0].*1Unitful.mm)
+  updateCoordinate(param, coord)
+end
+
+function setProtocolParameter(param::CoordinateParameter, params::ProtocolParams)
+  entryX = get_gtk_property(param["entX"], :text, String)
+  entryY = get_gtk_property(param["entY"], :text, String)
+  entryZ = get_gtk_property(param["entZ"], :text, String)
+  posFloat = tryparse.(Float64, [entryX, entryY, entryZ])
+  scannerCoord = ScannerCoords(posFloat.*1Unitful.mm)
+  setfield!(params, param.field, scannerCoord)
+end
