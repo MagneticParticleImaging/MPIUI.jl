011370720dda845bda97a853cc0f6b71d9da6570
nHackel <nHackel@users.noreply.github.com>
2022-09-15 18:05:46 +0200

    Add component display for ArbitraryElectricComponents

--- src/Protocol/SequenceParameter.jl
+++ src/Protocol/SequenceParameter.jl
@@ -92,6 +92,47 @@ mutable struct ComponentParameter <: Gtk.GtkGrid
     gridResult = new(grid.handle, idLabel, div, amp, pha, wav, waveforms)
     return Gtk.gobject_move_ref(gridResult, grid)
   end
+  function ComponentParameter(comp::ArbitraryElectricalComponent, waveforms::Vector{Waveform})
+    grid = GtkGrid()
+    set_gtk_property!(grid, :row_spacing, 5)
+    set_gtk_property!(grid, :column_spacing, 5)
+    # ID
+    idLabel = GtkLabel(id(comp), xalign = 0.0)
+    grid[1:2, 1] = idLabel
+    # Divider
+    div = GenericEntry{Int64}(string(divider(comp)))
+    set_gtk_property!(div, :sensitive, false)
+    grid[1, 2] = GtkLabel("Divider", xalign = 0.0)
+    grid[2, 2] = div
+    # Amplitude
+    ampVal = MPIMeasurements.amplitude(comp)
+    if ampVal isa typeof(1.0u"T")
+      ampVal = uconvert(u"mT", ampVal)
+    end
+    amp = UnitfulEntry(ampVal)
+    set_gtk_property!(amp, :sensitive, false)
+    grid[1, 3] = GtkLabel("Amplitude", xalign = 0.0)
+    grid[2, 3] = amp
+    # Phase
+    pha = UnitfulEntry(0.0u"rad") #UnitfulEntry(MPIMeasurements.phase(comp))
+    grid[1, 4] = GtkLabel("Phase", xalign = 0.0)
+    grid[2, 4] = pha
+
+    # Waveform
+    wav = ComboBoxTextLeaf()
+    waveformsStr = fromWaveform.(waveforms)
+    for w in waveformsStr
+      push!(wav, w)
+    end
+    activeIdx = findfirst(w->w == waveform(comp), waveforms)
+    set_gtk_property!(wav, :active, activeIdx-1) 
+    set_gtk_property!(wav, :sensitive, false) 
+    grid[1, 5] = GtkLabel("Waveform", xalign = 0.0)
+    grid[2, 5] = wav
+    gridResult = new(grid.handle, idLabel, div, amp, pha, wav, waveforms)
+    return Gtk.gobject_move_ref(gridResult, grid)
+  end
+
 end
 
 function initCallbacks(seqParam::SequenceParameter)
@@ -165,9 +206,12 @@ function setProtocolParameter(channelParam::PeriodicChannelParameter)
     component = channelParam.box[index]
     id = get_gtk_property(component.idLabel, :label, String)
     @info "Setting component $id"
-    amplitude!(channel, id, uconvert(u"T", value(component.amplitude)))
-    phase!(channel, id, value(component.phase))
-    wave = component.waveforms[get_gtk_property(component.waveform, :active, Int)+1]
-    waveform!(channel, id, wave)
+    # AWG is not sensitive atm, hacky distinction
+    if get_gtk_property(component.waveform, :sensitive, Bool)
+      amplitude!(channel, id, uconvert(u"T", value(component.amplitude)))
+      phase!(channel, id, value(component.phase))
+      wave = component.waveforms[get_gtk_property(component.waveform, :active, Int)+1]
+      waveform!(channel, id, wave)
+    end
   end
 end
