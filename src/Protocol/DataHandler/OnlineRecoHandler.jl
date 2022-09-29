d8108810245679b890c5fc01732e6dfc17623455
Tobias Knopp <tobias.knopp@tuhh.de>
2022-09-30 01:26:18 +0200

    Merge OnlineRecoWidget into OnlineRecoHandler

--- src/Protocol/DataHandler/OnlineRecoHandler.jl
+++ src/Protocol/DataHandler/OnlineRecoHandler.jl
@@ -1,25 +1,32 @@
 
 mutable struct OnlineRecoHandler <: AbstractDataHandler
   dataWidget::DataViewerWidget
-  onlineRecoWidget::OnlineRecoWidget
+  params::ReconstructionParameter
   @atomic enabled::Bool
   @atomic ready::Bool
   # Protocol
   deltaT::Float64
   bgMeas::Array{Float32, 4}
   oldUnit::String
+  systemMatrix
+  freq
+  recoGrid
 end
+
 
 function OnlineRecoHandler(scanner=nothing)
   dataWidget = DataViewerWidget()
-  onlineRecoWidget = OnlineRecoWidget(:test)
+  params = defaultRecoParams()
+  recoParams = ReconstructionParameter(params) 
+
 
   # Init Display Widget (warmstart)
   c = ones(Float32,1,3,3,3,1)
   c = makeAxisArray(c, [0.1,0.1,0.1], zeros(3), 1.0) 
   updateData!(dataWidget, ImageMeta(c))
 
-  return OnlineRecoHandler(dataWidget, onlineRecoWidget, true, true, 0, zeros(Float32,0,0,0,0), "")
+  return OnlineRecoHandler(dataWidget, recoParams, true, true, 0, zeros(Float32,0,0,0,0), "",
+                           nothing, nothing, nothing)
 end
 
 function init(handler::OnlineRecoHandler, protocol::Protocol)
@@ -38,7 +45,7 @@ function enable!(handler::OnlineRecoHandler, val::Bool)
   @atomic handler.enabled = val
 end
 getParameterTitle(handler::OnlineRecoHandler) = "Online Reco"
-getParameterWidget(handler::OnlineRecoHandler) = handler.onlineRecoWidget
+getParameterWidget(handler::OnlineRecoHandler) = handler.params
 getDisplayTitle(handler::OnlineRecoHandler) = "Online Reco"
 getDisplayWidget(handler::OnlineRecoHandler) = handler.dataWidget
 
@@ -72,6 +79,146 @@ function handleProgress(handler::OnlineRecoHandler, protocol::ContinousMeasureme
 end
 
 
+
+############
+
+
+# THIS is a copy from Offline Widget
+function updateSF(m::OnlineRecoHandler)
+  params = getParams(m.params)
+
+  bgcorrection = (params[:emptyMeasPath] != nothing) || params[:bgCorrectionInternal]
+                 
+  m.freq = filterFrequencies(m.params.bSF, minFreq=params[:minFreq], maxFreq=params[:maxFreq],
+                             SNRThresh=params[:SNRThresh], recChannels=params[:recChannels],
+                             numPeriodAverages = params[:numPeriodAverages], 
+                             numPeriodGrouping = params[:numPeriodGrouping],
+                             maxMixingOrder = params[:maxMixingOrder])
+
+
+  @info "Reloading SF"
+ 
+  m.systemMatrix, m.recoGrid = getSF(m.params.bSF, m.freq, params[:sparseTrafo], params[:solver], bgcorrection=bgcorrection,
+                      loadasreal = params[:loadasreal], loadas32bit = params[:loadas32bit],
+                      redFactor = params[:redFactor], numPeriodAverages = params[:numPeriodAverages], 
+                      numPeriodGrouping = params[:numPeriodGrouping], gridsize = params[:gridShape])
+
+  m.params.sfParamsChanged = false
+
+  return nothing
+end
+
+
+function reconstruct(m::OnlineRecoHandler, data)
+  @tspawnat 2 reconstruct_(m, data)
+  #execute_(m, data, dv)
+end
+
+@guarded function reconstruct_(m::OnlineRecoHandler, data)
+  
+  @info "Performing Reconstruction"
+
+  params = getParams(m.params)
+
+  if m.params.sfParamsChanged
+    updateSF(m)
+  end
+
+  @info "Size SM = $(size(m.systemMatrix)),   Size data = $(size(data))"
+
+  #if params[:emptyMeasPath] != nothing
+  #  params[:bEmpty] = MPIFile( params[:emptyMeasPath] )
+  #end
+
+  # If S is processed and fits not to the measurements because of numPeriodsGrouping
+  # or numPeriodAverages being applied we need to set these so that the 
+  # measurements are loaded correctly
+
+  @info "$(rxNumSamplingPoints(m.params.bSF[1]))   $(size(data,1))"
+
+  if rxNumSamplingPoints(m.params.bSF[1]) > size(data,1)
+    data = reshape(data, rxNumSamplingPoints(m.params.bSF[1]), size(data,2), :, size(data,3))
+  end
+
+  @info "Size SM = $(size(m.systemMatrix)),   Size data = $(size(data))"
+
+  @info "$(acqNumPeriodsPerFrame(m.params.bSF[1]))   $(size(data,3))"
+
+  if acqNumPeriodsPerFrame(m.params.bSF[1]) < size(data,3)
+    #params[:numPeriodAverages] = acqNumPeriodsPerFrame(m.bMeas) ÷ (acqNumPeriodsPerFrame(m.params.bSF[1]) * params[:numPeriodGrouping])
+    error("Implement me!")
+  end
+
+  @info "Size SM = $(size(m.systemMatrix)),   Size data = $(size(data))"
+
+  # apply FFT
+  if measIsFourierTransformed(m.params.bSF[1])
+    data = rfft(data, 1)
+  end
+
+  @info "Size SM = $(size(m.systemMatrix)),   Size data = $(size(data))"
+
+
+  #=if tfCorrection && !measIsTFCorrected(f)
+    tf = rxTransferFunction(f)
+    inductionFactor = rxInductionFactor(f)
+    data[2:end,:,:,:] ./= tf[2:end,:,:,:]
+    @warn "This measurement has been corrected with a Transfer Function. Name of TF: $(rxTransferFunctionFileName(f))"
+    if inductionFactor != nothing
+       	for k=1:length(inductionFactor)
+       		data[:,k,:,:] ./= inductionFactor[k]
+       	end
+    end
+  end =#
+
+  if m.freq != nothing
+    # here we merge frequencies and channels
+    data = reshape(data, size(data,1)*size(data,2), size(data,3), size(data,4))
+    data = data[m.freq, :, :]
+  end
+
+  @info "Size SM = $(size(m.systemMatrix)),   Size data = $(size(data))"
+
+  if eltype(m.systemMatrix) <: Real
+    data = MPIFiles.returnasreal(data)
+  end
+
+  data = reshape(data, :, size(data,3))
+
+  @info "Size SM = $(size(m.systemMatrix)),   Size data = $(size(data))"
+
+  c = MPIReco.reconstruction(m.systemMatrix, data; shape=shape(m.recoGrid), params...)
+
+  #conc = MPIReco.reconstruction(m.systemMatrix, m.params.bSF, m.bMeas, m.freq, m.recoGrid; params...)
+
+  D = shape(m.recoGrid)
+  numChan = length(m.params.bSF)
+  cB = permutedims(reshape(c, D[1]*D[2]*D[3], numChan),[2,1])
+  cF = reshape(cB, numChan, D[1], D[2], D[3], 1)
+
+  #images = cat(cF, images, dims=5)
+  cF = makeAxisArray(cF, spacing(m.recoGrid), m.recoGrid.center, 1.0) 
+
+  cF = ImageMeta(cF)
+
+  #m.recoResult = conc
+  #m.recoResult.recoParams = getParams(m.params)
+
+  @idle_add_guarded begin
+    updateData!(m.dataWidget, cF)
+  end
+
+  return nothing
+end
+
+
+
+
+
+
+##########
+
+
 function handleData(handler::OnlineRecoHandler, protocol::Protocol, event::DataAnswerEvent)
   data = event.data
   if isnothing(data)
@@ -84,7 +231,7 @@ function handleData(handler::OnlineRecoHandler, protocol::Protocol, event::DataA
     @atomic handler.ready = false
     @idle_add_guarded begin
       try
-        execute_(handler.onlineRecoWidget, data, handler.dataWidget)
+        reconstruct(handler, data)
       finally
         @atomic handler.ready = true
       end
