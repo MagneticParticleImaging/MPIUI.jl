72df0b09efae86aa370bbc41d24a5a3df9c82805
nHackel <nHackel@users.noreply.github.com>
2023-08-09 15:19:14 +0000

    Init connection from filter and list

--- src/Reconstruction/RecoPlanParameterList.jl
+++ src/Reconstruction/RecoPlanParameterList.jl
@@ -1,3 +1,132 @@
+# Based on https://github.com/JuliaGtk/Gtk4.jl/issues/29
+mutable struct RecoPlanParameterFilter
+  parameters::RecoPlanParameters
+  # Filter GTK elements
+  filterGrid::GtkGrid
+  view::Union{Nothing, GtkListView}
+  entry::GtkSearchEntry
+  missingButton::GtkCheckButton
+  # Filter State
+  dict::Dict{String, RecoPlanParameters}
+  planFilter::Dict{String, Bool}
+  # Hacky field to stop GTK segfaulting 
+  children::Vector{Vector{String}}
+end
+
+function RecoPlanParameterFilter(params::RecoPlanParameters)
+  dict = Dict{String, RecoPlanParameters}()
+  fillTreeDict!(dict, params)
+  planFilter = Dict{String, Bool}()
+  for key in keys(dict)
+    planFilter[key] = true
+  end
+  factory = GtkSignalListItemFactory()
+
+  filterGrid = GtkGrid()
+  entry = GtkSearchEntry()
+  entry.hexpand = true
+  missingButton = GtkCheckButton("Only missing")
+  missingButton.active = false
+  filter = RecoPlanParameterFilter(params, filterGrid, nothing, entry, missingButton, dict, planFilter, [String[]])
+
+  function create_tree(item, userData)
+    if item != C_NULL
+      itemLeaf = Gtk4.GLib.find_leaf_type(item)
+      item = convert(itemLeaf, item)
+      str = item.string
+
+      parent = filter.dict[str]
+      children = getChildStrings(parent) # Otherwise Strings seem to be GC'ed and segfaulst are caused
+      push!(filter.children, children)
+      store = GtkStringList(children)
+
+      return Gtk4.GLib.GListModel(store)
+    else
+      return C_NULL
+    end
+  end
+
+  function create_tree_raw(item, userData)
+    result = create_tree(item, userData)
+    return result.handle
+  end
+
+  function setup_tree(f, li)
+    expander = GtkTreeExpander()
+    box = GtkBox(:h)
+    set_child(expander, box)
+    set_child(li, expander)
+  end
+
+  function bind_tree(f, li)
+    row = li[]
+    if row != C_NULL
+      expander = get_child(li)
+      Gtk4.set_list_row(expander, row)
+      box = get_child(expander)
+      if isempty(box)
+        key = Gtk4.get_item(row).string
+        labelString = split(key, ".")[end]
+        label = GtkLabel(labelString)
+        label.hexpand = true
+        label.xalign = 0.0
+        check = GtkCheckButton()
+        check.active = filter.planFilter[key]
+        #expander.hide_expander = isempty(getChildStrings(filter.dict[key]))
+        # TODO set callback
+        push!(box, label)
+        push!(box, check)
+      end
+    end
+  end
+
+  function unbind_tree(f, li)
+  end
+
+  signal_connect(setup_tree, factory, "setup")
+  signal_connect(bind_tree, factory, "bind")
+  signal_connect(unbind_tree, factory, "unbind")
+
+
+  root = Gtk4.GLib.GListModel(GtkStringList(getChildStrings(params)))
+  tree = GtkTreeListModel(root, false, true, create_tree_raw)
+  selection = GtkSelectionModel(GtkSingleSelection(Gtk4.GLib.GListModel(tree)))
+  view = GtkListView(selection, factory)
+  view.hexpand = true
+  view.vexpand = true
+  filter.view = view
+
+  filterGrid[1, 1] = entry
+  filterGrid[2, 1] = missingButton
+  scroll = GtkScrolledWindow()
+  scroll[] = view
+  filterGrid[1:2, 2] = scroll
+  return filter
+end
+
+function fillTreeDict!(dict, params::RecoPlanParameters)
+  dict[id(params)] = params
+  for parameter in params.nestedParameters
+    fillTreeDict!(dict, parameter)
+  end
+  return dict
+end
+
+function getChildStrings(params::RecoPlanParameters)
+  result = String[]
+  for parameter in params.nestedParameters
+    push!(result, id(parameter))
+  end
+  return sort(result)
+end
+
+function match(filter::RecoPlanParameterFilter, li, user_data)
+  println(typeof(li))
+  result = true
+  return result == true ? Cint(1) : Cint(0)
+end
+
+
 mutable struct RecoPlanParameterList
   model::GtkStringList
   factory::GtkSignalListItemFactory
@@ -6,7 +135,7 @@ mutable struct RecoPlanParameterList
   dict::Dict{String, Union{RecoPlanParameter,RecoPlanParameters}}
 end
 
-function RecoPlanParameterList(params::RecoPlanParameters)
+function RecoPlanParameterList(params::RecoPlanParameters; filter::Union{RecoPlanParameterFilter, Nothing} = nothing)
   dict = Dict{String, Union{RecoPlanParameter,RecoPlanParameters}}()
   fillListDict!(dict, params)
   strings = sort(collect(keys(dict)))
@@ -30,6 +159,11 @@ function RecoPlanParameterList(params::RecoPlanParameters)
   end
   signal_connect(setup_param, factory, "setup")
   signal_connect(bind_param, factory, "bind")
+
+  if !isnothing(filter)
+    customFilter = GtkCustomFilter((li, data) -> match(filter, li, data))
+    model = GtkFilterListModel(GLib.GListModel(model), customFilter)
+  end
 
   view = GtkListView(GtkSelectionModel(GtkSingleSelection(GLib.GListModel(model))), factory)
   list.view = view
@@ -70,100 +204,4 @@ function getListChild(params::RecoPlanParameters{T}) where T
     grid[1:2, 1] = centerLabel
   end
   return grid
-end
-
-mutable struct RecoPlanParameterFilter
-  parameters::RecoPlanParameters
-  filterWidgets::Vector{GtkWidget}
-  dict::Dict{String, RecoPlanParameters}
-  view::Union{Nothing, GtkListView}
-end
-
-function RecoPlanParameterFilter(params::RecoPlanParameters)
-  dict = Dict{String, RecoPlanParameters}()
-  fillTreeDict!(dict, params)
-  factory = GtkSignalListItemFactory()
-
-  filter = RecoPlanParameterFilter(params, GtkWidget[], dict, nothing)
-
-  function create_tree(item, userData)
-    if item != C_NULL
-      itemLeaf = Gtk4.GLib.find_leaf_type(item)
-      println(itemLeaf)
-      item = convert(itemLeaf, item)
-      str = item.string
-      store = GtkStringList()
-
-      parent = filter.dict[str]
-      for child in getChildStrings(parent)
-        push!(store, child)
-      end
-
-      return Gtk4.GLib.GListModel(store)
-    else
-      return C_NULL
-    end
-  end
-
-  function create_tree_raw(item, userData)
-    result = create_tree(item, userData)
-    return result.handle
-  end
-
-  function setup_tree(f, li)
-    expander = GtkTreeExpander()
-    box = GtkBox(:h)
-    set_child(expander, box)
-    set_child(li, expander)
-  end
-
-  function bind_tree(f, li)
-    row = li[]
-    if row != C_NULL
-      expander = get_child(li)
-      Gtk4.set_list_row(expander, row)
-      box = get_child(expander)
-      if isempty(box)
-        labelString = split(Gtk4.get_item(row).string, ".")[end]
-        label = GtkLabel(labelString)
-        label.hexpand = true
-        label.xalign = 0.0
-        check = GtkCheckButton()
-        check.active = true
-        push!(box, label)
-        push!(box, check)
-      end
-    end
-  end
-
-  function unbind_tree(f, li)
-  end
-
-  signal_connect(setup_tree, factory, "setup")
-  signal_connect(bind_tree, factory, "bind")
-  signal_connect(unbind_tree, factory, "unbind")
-
-
-  root = Gtk4.GLib.GListModel(GtkStringList(getChildStrings(params)))
-  tree = GtkTreeListModel(root, false, true, create_tree_raw)
-  selection = GtkSelectionModel(GtkMultiSelection(Gtk4.GLib.GListModel(tree)))
-  view = GtkListView(selection, factory)
-  filter.view = view
-  return filter
-end
-
-function fillTreeDict!(dict, params::RecoPlanParameters)
-  dict[id(params)] = params
-  for parameter in params.nestedParameters
-    fillTreeDict!(dict, parameter)
-  end
-  return dict
-end
-
-function getChildStrings(params::RecoPlanParameters)
-  result = String[]
-  for parameter in params.nestedParameters
-    push!(result, id(parameter))
-  end
-  return sort(result)
 end
