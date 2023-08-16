00e8069fe87ae0d0c06cfcca892991f5a87c18ab
nHackel <nHackel@users.noreply.github.com>
2023-08-16 13:10:50 +0000

    Init custom widgets for regularization terms

--- src/Reconstruction/ParameterInputs/RegularizedLeastSquaresInputs.jl
+++ src/Reconstruction/ParameterInputs/RegularizedLeastSquaresInputs.jl
@@ -76,3 +76,116 @@ function update!(input::SolverNormalizationPlanInput, value::Missing)
   end
 end
 callback!(input::SolverNormalizationPlanInput, value) = input.cb = value
+
+abstract type AbstractRegularizationTermPlanInput <: RecoPlanParameterInput end
+
+mutable struct RegularizationTermPlanInput{R<:AbstractRegularization} <: AbstractRegularizationTermPlanInput
+  grid::GtkGrid
+  cb::Union{Nothing, Function}
+  inputs::Vector{RecoPlanParameterInput}
+  userChange::Bool
+  function RegularizationTermPlanInput(value::R, field::Union{Symbol, Nothing}) where R<:AbstractRegularization
+    grid = GtkGrid()
+    i = 1
+    grid[1, i] = GtkLabel(string(nameof(R)))
+    input = new{getfield(parentmodule(R), nameof(R))}(grid, nothing, RecoPlanParameterInput[], true)
+    if !isnothing(field)
+      grid[2, i] = GtkLabel(string(field))
+    end
+    i+=1
+    for field in fieldnames(R)
+      fieldInput = RecoPlanParameterInput(fieldtype(R, field), getfield(value, field), field)
+      callback!(fieldInput, () -> begin
+        if !isnothing(input.cb) && input.userChange
+          input.cb()
+        end 
+      end)
+      grid[1, i] = GtkLabel(string(field))
+      grid[2, i] = widget(fieldInput)
+      push!(input.inputs, fieldInput)
+      i+=1
+    end
+    return input
+  end
+end
+RecoPlanParameterInput(t::Type{T}, value::T, field) where T<:AbstractRegularization = RegularizationTermPlanInput(value, field)
+widget(input::RegularizationTermPlanInput) = input.grid
+function value(input::RegularizationTermPlanInput{R}) where R
+  values = Dict{Symbol, Any}()
+  for (idx, field) in enumerate(fieldnames(R))
+    values[field] = value(input.inputs[idx])
+  end
+  λ = pop!(values, :λ)
+  return R(λ; values...)
+end
+function update!(input::RegularizationTermPlanInput{R}, value::R) where R
+  input.userChange = false
+  for (idx, field) in enumerate(fieldnames(R))
+    update!(input.inputs[idx], getfield(value, field))
+  end
+  input.userChange = true
+end
+function update!(input::RegularizationTermPlanInput, value::Missing)
+  input.userChange = false
+  for fieldInput in input.inputs
+    update!(fieldInput, missing)
+  end
+  input.userChange = true
+end
+callback!(input::RegularizationTermPlanInput, value) = input.cb = value
+
+
+mutable struct RegularizationPlanInput <: RecoPlanParameterInput
+  list::GtkListBox
+  cb::Union{Nothing,Function}
+  regInputs::Vector{AbstractRegularizationTermPlanInput}
+  RegularizationPlanInput(value::Missing, field::Symbol) = RegularizationPlanInput(AbstractRegularization[], field)
+  function RegularizationPlanInput(value::Vector{<:AbstractRegularization}, field::Symbol)
+    list = GtkListBox()
+    list.vexpand = true
+    list.hexpand = true
+    list.show_separators = true
+    input = new(list, nothing, AbstractRegularizationTermPlanInput[])
+    update!(input, value)
+    return input
+  end
+end
+RecoPlanParameterInput(::Type{Vector{R} where R<:AbstractRegularization}, value, field) = RegularizationPlanInput(value, field)
+widget(input::RegularizationPlanInput) = input.list
+function value(input::RegularizationPlanInput)
+  result = AbstractRegularization[]
+  for input in input.regInputs
+    push!(result, value(input))
+  end
+  return isempty(result) ? missing : result
+end
+function update!(input::RegularizationPlanInput, value)
+  widgets = Any[]
+  for widget in input.list
+    push!(widgets, widget)
+  end
+  for widget in widgets
+    delete!(input.list, widget)
+  end
+  empty!(input.regInputs)
+  for reg in value
+    regInput = RegularizationTermPlanInput(reg, nothing)
+    callback!(regInput, () -> begin
+      if !isnothing(input.cb)
+        input.cb()
+      end 
+    end)
+    push!(input.regInputs, regInput)
+    push!(input.list, widget(regInput))
+  end
+end
+function update!(input::RegularizationPlanInput, value::Missing)
+  for widget in input.list
+    push!(widgets, widget)
+  end
+  for widget in widgets
+    delete!(input.list, widget)
+  end
+  empty!(input.regInputs)
+end
+callback!(input::RegularizationPlanInput, value) = input.cb = value
