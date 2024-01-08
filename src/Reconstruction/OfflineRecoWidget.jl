e8fb34ce5c042aa2a1edca550f801073d2dbbf76
nHackel <nHackel@users.noreply.github.com>
2024-01-08 15:47:00 +0100

    Init updating offline reco widget

--- src/Reconstruction/OfflineRecoWidget.jl
+++ src/Reconstruction/OfflineRecoWidget.jl
@@ -8,7 +8,11 @@ include("PlanRecoWidget.jl")
 mutable struct OfflineRecoWidget <: Gtk4.GtkGrid
   handle::Ptr{Gtk4.GObject}
   builder
-  params::ReconstructionParameter
+  plandd::GtkDropDown
+  plan::Union{Nothing, RecoPlan}
+  inputs::Union{Nothing, RecoPlanParameters}
+  filter::Union{Nothing, RecoPlanParameterFilter}
+  list::Union{Nothing, RecoPlanParameterList}
   dv
   bMeas
   sysMatrix
@@ -26,9 +30,9 @@ mutable struct RecoWindow
   rw::OfflineRecoWidget
 end
 
-function RecoWindow(filenameMeas=nothing; params = defaultRecoParams())
+function RecoWindow(filenameMeas=nothing; plan = nothing)
   w = GtkWindow("Reconstruction",800,600)
-  dw = OfflineRecoWidget(filenameMeas, params=params)
+  dw = OfflineRecoWidget(filenameMeas, plan = plan)
   push!(w,dw)
 
   #if isassigned(mpilab)
@@ -40,35 +44,35 @@ function RecoWindow(filenameMeas=nothing; params = defaultRecoParams())
   return RecoWindow(w,dw)
 end
 
-function OfflineRecoWidget(filenameMeas=nothing; params = defaultRecoParams())
+function OfflineRecoWidget(filenameMeas=nothing; plan = nothing)
 
   uifile = joinpath(@__DIR__, "..", "builder", "reconstructionWidget.ui")
 
   b = GtkBuilder(uifile)
   mainGrid = G_.get_object(b, "gridReco")
-  boxParams = G_.get_object(b, "boxParams")
-
-  recoParams = ReconstructionParameter(params)
-
-  push!(boxParams, recoParams)
-###  set_gtk_property!(boxParams,:fill,recoParams, true) 
-###  set_gtk_property!(boxParams,:expand,recoParams, true) 
-  show(recoParams) 
-
   @info "in OfflineRecoWidget"
 
-  m = OfflineRecoWidget( mainGrid.handle, b,
-                  recoParams,
+  choices = map(x -> split(x, ".toml")[1], filter(contains(".toml"), readdir(AbstractImageReconstruction.plandir(MPIReco))))
+  plandd = GtkDropDown(choices)
+  Gtk4.enable_search(plandd, true)
+
+  m = OfflineRecoWidget( mainGrid.handle, b, plandd,
+                  plan, nothing, nothing, nothing,
                   nothing,
                   nothing,
                   nothing, nothing, nothing, nothing,
                   nothing, nothing)
   Gtk4.GLib.gobject_move_ref(m, mainGrid)
 
+  # Add plan handling
+  pushfirst!(m["tbRec"], plandd)
+  if isnothing(plan)
+    plan = Gtk4.selected_string(plandd)
+    updatePlan!(m, plan)
+  end
+
   m.dv = DataViewerWidget()
   push!(m["boxDW"], m.dv)
-  ###set_gtk_property!(m["boxDW"], :fill, m.dv, true)
-  ###set_gtk_property!(m["boxDW"], :expand, m.dv, true)
 
   updateData!(m, filenameMeas)
 
@@ -80,18 +84,11 @@ end
 function initCallbacks(m::OfflineRecoWidget)
   signal_connect((w)->performReco(m), m["tbPerformReco"], "clicked")
   signal_connect((w)->saveReco(m), m["tbSaveReco"], "clicked")
-end
-
-function setParams(m::OfflineRecoWidget, params)
-  return setParams(m.params, params)
-end
-
-function getParams(m::OfflineRecoWidget)
-  return getParams(m.params)
-end
-
-function setSF(m::OfflineRecoWidget, path)
-  return setSF(m.params, path)
+  signal_connect(m.plandd, "notify::selected") do widget, others...
+    plan = Gtk4.selected_string(m.plandd)
+    updatePlan!(m, plan)
+  end
+  
 end
 
 function saveReco(m::OfflineRecoWidget)
@@ -113,57 +110,38 @@ function saveReco(m::OfflineRecoWidget)
   end
 end
 
-function updateData!(m::OfflineRecoWidget, filenameMeas, params::Dict,
+updatePlan!(m::OfflineRecoWidget, plan::String) = updatePlan!(m, loadPlan(MPIReco, plan, [MPIReco, MPIFiles, RegularizedLeastSquares, AbstractImageReconstruction]))
+function updatePlan!(m::OfflineRecoWidget, plan::RecoPlan)
+  @idle_add_guarded begin 
+    boxParams = m["boxParams"]
+    empty!(boxParams)
+
+    inputs = RecoPlanParameters(plan)
+    filter = RecoPlanParameterFilter(inputs)
+    list = RecoPlanParameterList(inputs, filter = filter)
+    recoPanel = GtkPaned(:h)
+    recoPanel[1] = widget(filter)
+    sw = GtkScrolledWindow()
+    sw[] = widget(list)
+    recoPanel[2] = sw
+
+    push!(boxParams, recoPanel)
+    m.plan = plan
+  end
+end
+
+function updateData!(m::OfflineRecoWidget, filenameMeas, plan::RecoPlan,
                      study=nothing, experiment=nothing)
-  setParams(m.params, params)
+  updatePlan!(m, plan)
   updateData!(m, filenameMeas, study, experiment)
 end
 
 @guarded function updateData!(m::OfflineRecoWidget, filenameMeas, study=nothing, experiment=nothing)
   if filenameMeas != nothing
     m.bMeas = MPIFile(filenameMeas)
-    set_gtk_property!(m.params["adjFrame"],:upper, acqNumFrames(m.bMeas))
-    set_gtk_property!(m.params["adjLastFrame"],:upper, acqNumFrames(m.bMeas))
-    try
-      if filepath(m.params.bSF[1])=="" #&& isdir( sfPath(m.bMeas) )
-        #setSF(m, sfPath(m.bMeas)  )
-      elseif isdir( filepath(m.params.bSF[1]) ) || isfile( filepath(m.params.bSF[1]) )
-        setSF(m.params, filepath(m.params.bSF[1]) )
-      end
-    catch e
-      @show e
-    end
-    initBGSubtractionWidgets(m.params, study, experiment)
     m.currentStudy = study
     m.currentExperiment = experiment
   end
-  return nothing
-end
-
-function updateSF(m::OfflineRecoWidget)
-  params = getParams(m.params)
-
-  bgcorrection = (params[:emptyMeasPath] != nothing) || params[:bgCorrectionInternal]
-                 
-  m.freq = filterFrequencies(m.params.bSF, minFreq=params[:minFreq], maxFreq=params[:maxFreq],
-                             SNRThresh=params[:SNRThresh], recChannels=params[:recChannels],
-                             numPeriodAverages = params[:numPeriodAverages], 
-                             numPeriodGrouping = params[:numPeriodGrouping],
-                             maxMixingOrder = params[:maxMixingOrder])
-
-
-  @info "Reloading SF"
-  @show m.params.bSF
-  infoMessage(m, "Loading System Matrix...")
-
-  m.sysMatrix, m.recoGrid = getSF(m.params.bSF, m.freq, params[:sparseTrafo], params[:solver], bgcorrection=bgcorrection,
-                      loadasreal = params[:loadasreal], loadas32bit = params[:loadas32bit],
-                      redFactor = params[:redFactor], numPeriodAverages = params[:numPeriodAverages], 
-                      numPeriodGrouping = params[:numPeriodGrouping], gridsize = params[:gridShape])
-
-  infoMessage(m, "")
-  m.params.sfParamsChanged = false
-
   return nothing
 end
 
@@ -188,51 +166,14 @@ function performReco(m::OfflineRecoWidget)
 end
 
 @guarded function performReco_(m::OfflineRecoWidget)
-  
-  params = getParams(m.params)
-
-  @idle_add_guarded progress(m, true)
-
-  if m.params.sfParamsChanged
-    updateSF(m)
-  end
-
-  if params[:emptyMeasPath] != nothing
-    params[:bEmpty] = MPIFile( params[:emptyMeasPath] )
-    if typeof(params[:bEmpty]) == BrukerFileCalib
-      bbEmpty = params[:bEmpty]
-      bbEmpty_ = BrukerFileMeas(bbEmpty.path,bbEmpty.params,bbEmpty.paramsProc,bbEmpty.methodRead,bbEmpty.acqpRead,bbEmpty.visupars_globalRead,bbEmpty.recoRead,bbEmpty.methrecoRead,bbEmpty.visuparsRead,bbEmpty.mpiParRead,bbEmpty.maxEntriesAcqp);
-      params[:bEmpty] = bbEmpty_
-    end
-  end
-
-  # If S is processed and fits not to the measurements because of numPeriodsGrouping
-  # or numPeriodAverages being applied we need to set these so that the 
-  # measurements are loaded correctly
-  if rxNumSamplingPoints(m.params.bSF[1]) > rxNumSamplingPoints(m.bMeas)
-    params[:numPeriodGrouping] = rxNumSamplingPoints(m.params.bSF[1]) ÷ rxNumSamplingPoints(m.bMeas)
-  end
-  if acqNumPeriodsPerFrame(m.params.bSF[1]) < acqNumPeriodsPerFrame(m.bMeas)
-    params[:numPeriodAverages] = acqNumPeriodsPerFrame(m.bMeas) ÷ (acqNumPeriodsPerFrame(m.params.bSF[1]) * params[:numPeriodGrouping])
-  end
-
-  infoMessage(m, "Performing Reconstruction...")
-
-  if typeof(m.bMeas) == BrukerFileCalib
-    m.bMeas = BrukerFileMeas(m.bMeas.path,m.bMeas.params,m.bMeas.paramsProc,m.bMeas.methodRead,m.bMeas.acqpRead,m.bMeas.visupars_globalRead,m.bMeas.recoRead,m.bMeas.methrecoRead,m.bMeas.visuparsRead,m.bMeas.mpiParRead,m.bMeas.maxEntriesAcqp);
-
-    conc = MPIReco.reconstruction(m.sysMatrix, m.params.bSF, m.bMeas, m.freq, m.recoGrid; params...)
-
-  else
-    conc = MPIReco.reconstruction(m.sysMatrix, m.params.bSF, m.bMeas, m.freq, m.recoGrid; params...)
-  end
-  m.recoResult = conc
-  m.recoResult.recoParams = getParams(m.params)
+  algo = build(m.plan)
+  progress(m, true)
+  m.recoResult = MPIReco.reconstruct(algo, m.bMeas)
+  #m.recoResult.recoParams = getParams(m.params)
   @idle_add_guarded begin
     updateData!(m.dv, m.recoResult)
     infoMessage(m, "")
     progress(m, false)
   end
-
   return nothing
 end
